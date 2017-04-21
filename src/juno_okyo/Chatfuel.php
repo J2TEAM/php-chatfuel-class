f999229a1a08be759f1f47154fc9fe8d7e564f61
Nguyen Yustar <yune.love@gmail.com>
2017-04-21 18:41:59 +0700

    Minor update
    - Reformat code to PSR2
    
    - Refactor code using 'return first' technique
    
    - Add tests

--- src/juno_okyo/Chatfuel.php
+++ src/juno_okyo/Chatfuel.php
@@ -1,233 +1,251 @@
 <?php
+
 /**
  * Copyright 2017 Juno_okyo <junookyo@gmail.com>
  *
  * Website: https://junookyo.blogspot.com/
  */
+
 namespace juno_okyo;
 
 class Chatfuel
 {
-  const VERSION = '1.0.0';
+    const VERSION = '1.0.1';
 
-  protected $response = array();
+    protected $response = array();
 
-  public function __construct($debug = FALSE)
-  {
-    if (( ! $debug) && ( ! isset($_SERVER['HTTP_USER_AGENT']) OR strpos($_SERVER['HTTP_USER_AGENT'], 'Apache-HttpAsyncClient') === FALSE)) {
-      exit;
-    }
-  }
-
-  public function __destruct()
-  {
-    if (count($this->response) > 0) {
-      try {
-        header('Content-Type: application/json');
-        echo json_encode(array('messages' => $this->response));
-        exit;
-      } catch (Exception $e) {
-        // noop
-      }
-    }
-  }
-
-  public function sendText($messages = null)
-  {
-    if (is_null($messages)) {
-      throw new Exception('Invalid input', 1);
+    public function __construct($debug = false)
+    {
+        if ((! $debug) && (! isset($_SERVER['HTTP_USER_AGENT']) || strpos($_SERVER['HTTP_USER_AGENT'], 'Apache-HttpAsyncClient') === false)) {
+            exit;
+        }
     }
 
-    $type = gettype($messages);
-    if ($type === 'string') {
-      $this->response[] = array('text' => $messages);
-    } elseif ($type === 'array' || is_array($messages)) {
-      foreach ($messages as $message) {
-        $this->response[] = array('text' => $message);
-      }
-    } else {
-      $this->response[] = array('text' => 'Error!');
-    }
-  }
+    public function __destruct()
+    {
+        // never run when execute PHPUnit
+        if (count($this->response) > 0 && ! $this->isCLI()) {
+            try {
+                header('Content-Type: application/json');
+                echo json_encode(array('messages' => $this->response));
 
-  public function sendImage($url)
-  {
-    if ($this->isURL($url)) {
-      $this->sendAttachment('image', array('url' => $url));
-    } else {
-      $this->sendText('Error: Invalid URL!');
-    }
-  }
-
-  public function sendVideo($url)
-  {
-    if ($this->isURL($url)) {
-      $this->sendAttachment('video', array('url' => $url));
-    } else {
-      $this->sendText('Error: Invalid URL!');
-    }
-  }
-
-  public function sendAudio($url)
-  {
-    if ($this->isURL($url)) {
-      $this->sendAttachment('audio', array('url' => $url));
-    } else {
-      $this->sendText('Error: Invalid URL!');
-    }
-  }
-
-  public function sendTextCard($text, $buttons)
-  {
-    if (is_array($buttons)) {
-      $this->sendAttachment('template', array(
-        'template_type' => 'button',
-        'text'          => $text,
-        'buttons'       => $buttons
-      ));
-
-      return TRUE;
+                exit;
+            } catch (\Exception $e) {
+                // nop
+            }
+        }
     }
 
-    return FALSE;
-  }
+    public function sendText($messages = null)
+    {
+        if (is_null($messages)) {
+            throw new \Exception('Invalid input', 1);
+        }
 
-  public function sendGallery($elements)
-  {
-    if (is_array($elements)) {
-      $this->sendAttachment('template', array(
-        'template_type' => 'generic',
-        'elements'      => $elements
-      ));
+        if (! is_string($messages) && ! is_array($messages)) {
+            return $this->response[] = array('text' => 'Error!');
+        }
 
-      return TRUE;
+        if (is_array($messages)) {
+            $arr = array();
+
+            foreach ($messages as $message) {
+                $arr[] = array('text' => $message);
+            }
+
+            $this->response = array_merge($this->response, $arr);
+
+            return $arr;
+        }
+
+        return $this->response[] = array('text' => $messages);
     }
 
-    return FALSE;
-  }
+    public function sendImage($url)
+    {
+        if (! $this->isURL($url)) {
+            return $this->sendText('Error: Invalid URL!');
+        }
 
-  public function createElement($title, $image, $subTitle, $buttons)
-  {
-    if ($this->isURL($image) && is_array($buttons)) {
-      return array(
-        'title'     => $title,
-        'image_url' => $image,
-        'subtitle'  => $subTitle,
-        'buttons'   => $buttons
-      );
+        return $this->sendAttachment('image', array('url' => $url));
     }
 
-    return FALSE;
-  }
+    public function sendVideo($url)
+    {
+        if (! $this->isURL($url)) {
+            return $this->sendText('Error: Invalid URL!');
+        }
 
-  public function createButtonToBlock($title, $block, $setAttributes = NULL)
-  {
-    $button = array();
-    $button['type'] = 'show_block';
-    $button['title'] = $title;
-    
-    if (is_array($block)) {
-      $button['block_names'] = $block;
-    } else {
-      $button['block_name'] = $block;
+        return $this->sendAttachment('video', array('url' => $url));
     }
 
-    if ( ! is_null($setAttributes) && is_array($setAttributes)) {
-      $button['set_attributes'] = $setAttributes;
+    public function sendAudio($url)
+    {
+        if (! $this->isURL($url)) {
+            return $this->sendText('Error: Invalid URL!');
+        }
+
+        return $this->sendAttachment('audio', array('url' => $url));
     }
 
-    return $button;
-  }
+    public function sendTextCard($text, $buttons)
+    {
+        if (! is_array($buttons)) {
+            return;
+        }
 
-  public function createButtonToURL($title, $url, $setAttributes = NULL)
-  {
-    if ($this->isURL($url)) {
-      $button = array();
-      $button['type'] = 'web_url';
-      $button['url'] = $url;
-      $button['title'] = $title;
-      
-      if ( ! is_null($setAttributes) && is_array($setAttributes)) {
-        $button['set_attributes'] = $setAttributes;
-      }
-
-      return $button;
+        return $this->sendAttachment('template', array(
+            'template_type' => 'button',
+            'text'          => $text,
+            'buttons'       => $buttons
+        ));
     }
 
-    return FALSE;
-  }
+    public function sendGallery($elements)
+    {
+        if (! is_array($elements)) {
+            return;
+        }
 
-  public function createPostBackButton($title, $url)
-  {
-    if ($this->isURL($url)) {
-      return array(
-        'url'   => $url,
-        'type'  => 'json_plugin_url',
-        'title' => $title
-      );
+        return $this->sendAttachment('template', array(
+            'template_type' => 'generic',
+            'elements'      => $elements
+        ));
     }
 
-    return FALSE;
-  }
+    public function createElement($title, $image, $subTitle, $buttons)
+    {
+        if (! $this->isURL($image) || ! is_array($buttons)) {
+            return;
+        }
 
-  public function createCallButton($phoneNumber, $title = 'Call')
-  {
-    return array(
-      'type'         => 'phone_number',
-      'phone_number' => $phoneNumber,
-      'title'        => $title
-    );
-  }
-
-  public function createShareButton()
-  {
-    return array('type' => 'element_share');
-  }
-
-  public function createQuickReply($text, $quickReplies)
-  {
-    if (is_array($quickReplies)) {
-      $this->response['text'] = $text;
-      $this->response['quick_replies'] = $quickReplies;
-      return TRUE;
+        return array(
+            'title'     => $title,
+            'image_url' => $image,
+            'subtitle'  => $subTitle,
+            'buttons'   => $buttons
+        );
     }
 
-    return FALSE;
-  }
+    public function createButtonToBlock($title, $block, $setAttributes = null)
+    {
+        $button = array();
+        $button['type'] = 'show_block';
+        $button['title'] = $title;
 
-  public function createQuickReplyButton($title, $block)
-  {
-    $button = array();
-    $button['title'] = $title;
+        if (is_array($block)) {
+            $button['block_names'] = $block;
+        }
 
-    if (is_array($block)) {
-      $button['block_names'] = $block;
-    } else {
-      $button['block_name'] = $block;
+        if (is_string($block)) {
+            $button['block_name'] = $block;
+        }
+
+        if (! is_null($setAttributes) && is_array($setAttributes)) {
+            $button['set_attributes'] = $setAttributes;
+        }
+
+        return $button;
     }
 
-    return $button;
-  }
+    public function createButtonToURL($title, $url, $setAttributes = null)
+    {
+        if (! $this->isURL($url)) {
+            return;
+        }
 
-  private function sendAttachment($type, $payload)
-  {
-    $type = strtolower($type);
-    $validTypes = array('image', 'video', 'audio', 'template');
+        $button = array();
+        $button['type'] = 'web_url';
+        $button['url'] = $url;
+        $button['title'] = $title;
 
-    if (in_array($type, $validTypes)) {
-      $this->response[] = array(
-        'attachment' => array(
-          'type'    => $type,
-          'payload' => $payload
-        )
-      );
-    } else {
-      $this->response[] = array('text' => 'Error: Invalid type!');
+        if (! is_null($setAttributes) && is_array($setAttributes)) {
+            $button['set_attributes'] = $setAttributes;
+        }
+
+        return $button;
     }
-  }
 
-  private function isURL($url)
-  {
-    return filter_var($url, FILTER_VALIDATE_URL);
-  }
+    public function createPostBackButton($title, $url)
+    {
+        if (! $this->isURL($url)) {
+            return;
+        }
+
+        return array(
+            'url'   => $url,
+            'type'  => 'json_plugin_url',
+            'title' => $title
+        );
+    }
+
+    public function createCallButton($phoneNumber, $title = 'Call')
+    {
+        return array(
+            'type'         => 'phone_number',
+            'phone_number' => $phoneNumber,
+            'title'        => $title
+        );
+    }
+
+    public function createShareButton()
+    {
+        return array('type' => 'element_share');
+    }
+
+    public function createQuickReply($text, $quickReplies)
+    {
+        if (! is_array($quickReplies)) {
+            return;
+        }
+
+        $arr = array('text' => $text, 'quick_replies' => $quickReplies);
+
+        $this->response = array_merge($this->response, $arr);
+
+        return $arr;
+    }
+
+    public function createQuickReplyButton($title, $block)
+    {
+        $button = array();
+        $button['title'] = $title;
+
+        if (is_array($block)) {
+            $button['block_names'] = $block;
+        }
+
+        if (is_string($block)) {
+            $button['block_name'] = $block;
+        }
+
+        return $button;
+    }
+
+    private function sendAttachment($type, $payload)
+    {
+        $type = strtolower($type);
+        $validTypes = array('image', 'video', 'audio', 'template');
+
+        if (! in_array($type, $validTypes)) {
+            return $this->response[] = array('text' => 'Error: Invalid type!');
+        }
+
+        return $this->response[] = array(
+            'attachment' => array(
+                'type'    => $type,
+                'payload' => $payload
+            )
+        );
+    }
+
+    private function isCLI()
+    {
+        return php_sapi_name() === 'cli';
+    }
+
+    private function isURL($url)
+    {
+        return filter_var($url, FILTER_VALIDATE_URL);
+    }
 }
